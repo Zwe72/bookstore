c4c2882a5339531032647511b6ade5686c1f661e
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-01-27 13:57:36 +0200

    kesken

--- src/main/java/fi/haagahelia/controller/BookController.java
+++ src/main/java/fi/haagahelia/controller/BookController.java
@@ -1,11 +1,17 @@
 package fi.haagahelia.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import fi.haagahelia.model.Book;
 
 @Controller
 public class BookController {
+    private List<Book> books = new ArrayList<>();
     @GetMapping("/naytalomake")
     public String naytaLomake(Model model){
 
@@ -13,3 +19,17 @@ public class BookController {
         return "booklist";
     }
 }
+
+@PostMapping("/naytalomake")
+public String naytaArvot(@ModelAttribute Book book, Model model) {
+    model.addAttribute(model)
+    return "booklist";
+}
+@PostMapping("/naytalomake")
+    public String naytaArvot(ModelAttribute Book book, Model model) {
+        model.addAttribute("insertedbook", book);
+        add.List()
+
+
+    }
+)
