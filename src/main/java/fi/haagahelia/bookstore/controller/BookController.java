0650d477e4c087aff215e0b80f038ef957c55456
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-02-03 03:38:54 +0200

    Updated

---
+++ src/main/java/fi/haagahelia/bookstore/controller/BookController.java
@@ -0,0 +1,17 @@
+package fi.haagahelia.bookstore.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class BookController {
+    
+    @RequestMapping("/index")
+        public String showIndex() {
+            return "index";
+        }
+}
+
