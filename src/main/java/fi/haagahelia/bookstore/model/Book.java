a4da7dd499acdedb4a28e4ae098432249041a7d9
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-02-10 13:42:47 +0200

    tein tunnissa

--- src/main/java/fi/haagahelia/bookstore/model/Book.java
+++ src/main/java/fi/haagahelia/bookstore/model/Book.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -15,10 +17,15 @@ public class Book {
     private int publicationYear;
     private String isbn;
     private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Category category;
     
     public Book() {}
 
-    public Book(String title, String author, int publicationYear, String isbn, double price) {
+    public Book(String title, String author, int publicationYear, String isbn, double price, Category  category) {
+        super();
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
@@ -35,6 +42,10 @@ public class Book {
     public String getAuthor() {
         return author;
     }
+    public Book(Category category) {
+        this.category = category;
+    }
+
     public int getPublicationYear() {
         return publicationYear;
     }
@@ -43,6 +54,14 @@ public class Book {
     }
     public double getPrice() {
         return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public void setId(Long id) {
