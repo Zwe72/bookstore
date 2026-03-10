6fcf021f1425cce6988b575ff4675bbb3f7272d5
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-03-10 13:41:47 +0200

    tunnilla tehty

---
+++ src/test/java/fi/haagahelia/bookstore/BookRepositoryTest.java
@@ -0,0 +1,19 @@
+package fi.haagahelia.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fi.haagahelia.bookstore.model.BookRepository;
+import fi.haagahelia.bookstore.model.Category;
+
+public class BookRepositoryTest {
+    @Autowired
+    private BookRepository repository;
+
+    @Test
+    public void createNewBook() {
+        Category category = new Category("Fantasy");
+        drepository.save(category);
+    }
+
+}
