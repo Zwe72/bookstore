1d14f73131568ea6cadff0edcd08324c049c61bd
bhm526@myy.haaga-helia.fi <bhm526@myy.haaga-helia.fi>
2026-03-10 04:13:10 +0200

    c6.2 done

--- src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/bookstore/WebSecurityConfig.java
@@ -3,9 +3,8 @@ package fi.haagahelia.bookstore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +14,6 @@ public class WebSecurityConfig {
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/", "/home").permitAll()
                 .requestMatchers("/deletebook/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
@@ -24,26 +22,13 @@ public class WebSecurityConfig {
             .defaultSuccessUrl("/booklist", true)
             .permitAll()
         )
-        .logout( logout -> logout
-            .permitAll()
-        );
+        .logout( logout -> logout.permitAll());
+
         return http.build();
     } 
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
-        
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
